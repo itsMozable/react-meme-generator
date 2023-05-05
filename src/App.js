cbe0d350bfad1184bb6a62faa0f42b184d4744f2
tattoomozi <tattoomozi@icloud.com>
2023-05-05 16:52:34 +0200

    Added first yet not working draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
 import './App.css';
-import logo from './logo.svg';
+import { saveAs } from 'file-saver';
+import { useState } from 'react';
+import { memeTemps } from './memes.js';
+import defaultDoge from './memeTemplates/doge.jpeg';
 
 export default function App() {
+  // const [memeTemplates, setMemeTemplates] = useState('honest');
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [meme, setMeme] = useState(defaultDoge);
+  const [finalMeme, setFinalMeme] = useState(
+    `https://api.memegen.link/images/${meme}/${topText}/${bottomText}`,
+  );
+  // Create final Meme - which wont work because i get no response
+  function generateMeme(event) {
+    event.preventDefault();
+    setFinalMeme(
+      `https://api.memegen.link/images/${meme}/${topText}/${bottomText}`,
+    );
+  }
+
+  // Download and save Meme
+  const downloadMeme = () => {
+    const url = finalMeme;
+    saveAs(url, meme); // ???? save as importiert- noch ka was wie wo
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div align="center">
+      <h1>Meme Generator</h1>
+
+      {/* Top Text */}
+      <form>
+        <label>
+          Top text:
+          <input
+            value={topText}
+            onChange={(event) => {
+              setTopText(event.target.value);
+            }}
+          />
+        </label>
+      </form>
+      {/* Bottom Text */}
+      <form>
+        <label>
+          Bottom text:
+          <input
+            value={bottomText}
+            onChange={(event) => {
+              setBottomText(event.target.value);
+            }}
+          />
+        </label>
+      </form>
+      {/* Picture for the meme
+      Default meme works but the select does not work. Don't know why.
+      */}
+      <p>Juice your fresh picture !</p>
+      <form>
+        <div>
+          Meme Templates:
+          <select
+            defaultValue={defaultDoge}
+            value={meme}
+            onChange={(e) => {
+              setMeme(e.target.value);
+            }}
+          >
+            {/* derzeit noch Url statt label */}
+            {memeTemps.map((choice) => (
+              <option value={choice.value} key={choice}>
+                {choice.value}
+              </option>
+            ))}
+          </select>
+          <img src={meme} alt="wtf" />
+        </div>
+      </form>
+      <div>
+        <button onClick={generateMeme}>Generate Meme</button>
+        <button onClick={downloadMeme}>Download Meme</button>
+      </div>
     </div>
   );
 }
---
+++ src/memes.js
@@ -0,0 +1,24 @@
+import agnes from './memeTemplates/agnes.jpeg';
+import captain from './memeTemplates/captain.jpeg';
+import depressed from './memeTemplates/depressed.jpeg';
+import doge from './memeTemplates/doge.jpeg';
+import grumpy from './memeTemplates/grumpy.jpg';
+import honest from './memeTemplates/honest.jpeg';
+import kermit from './memeTemplates/kermit.jpeg';
+import noone from './memeTemplates/noone.jpg';
+import scared from './memeTemplates/scared.jpg';
+import wonka from './memeTemplates/wonka.jpg';
+
+// Creating an Array from Pictures
+export const memeTemps = [
+  { label: 'agnes', value: agnes },
+  { label: 'captain', value: captain },
+  { label: 'depressed', value: depressed },
+  { label: 'doge', value: doge },
+  { label: 'grumpy', value: grumpy },
+  { label: 'honest', value: honest },
+  { label: 'kermit', value: kermit },
+  { label: 'noone', value: noone },
+  { label: 'scared', value: scared },
+  { label: 'wonka', value: wonka },
+];
