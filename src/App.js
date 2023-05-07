266633b24cae4eb02089d9d01f39a7da8405d4a4
tattoomozi <tattoomozi@icloud.com>
2023-05-07 19:32:30 +0200

    pushing not finished version

--- src/App.js
+++ src/App.js
@@ -1,74 +1,132 @@
-import './App.css';
-import React, { useState } from 'react';
+import './styles.css';
+import { saveAs } from 'file-saver';
+import React, { useEffect, useState } from 'react';
 
 export default function MemeGenerator() {
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
-  const [meme, setMeme] = useState('doge');
+  const [topText, setTopText] = useState(' ');
+  const [bottomText, setBottomText] = useState(' ');
+  const [options, setOptions] = useState([]);
+  const [meme, setMeme] = useState('fry');
   const [finalMeme, setFinalMeme] = useState(
     `https://api.memegen.link/images/${meme}/${topText}/${bottomText}`,
   );
+
+  // fetch all the meme templates
+  useEffect(() => {
+    fetch(`https://api.memegen.link/templates`)
+      .then((response) => response.json())
+      .then((data) => {
+        setOptions(data);
+        console.log(data);
+      });
+  }, []);
+
+  function clearText(event) {
+    event.preventDefault();
+    setTopText('');
+    setBottomText('');
+  }
+  // generate final meme
   function generateMeme(event) {
     event.preventDefault();
-    setFinalMeme(
-      `https://api.memegen.link/images/${meme}/${topText}/${bottomText}`,
-    );
-    console.log(finalMeme);
+    if (topText === '' || bottomText === '') {
+      setFinalMeme(`https://api.memegen.link/images/${meme}/ / `);
+    } else {
+      setFinalMeme(
+        `https://api.memegen.link/images/${meme}/${topText}/${bottomText}`,
+      );
+      console.log(finalMeme);
+    }
   }
+
+  // Download and save Meme
+  const downloadMeme = () => {
+    const url = finalMeme;
+    saveAs(url, meme);
+  };
+
   return (
-    <div className="GeneratorButton">
-      <h1>Meme Generator</h1>
-      <p>Juice your fresh picture !</p>
-      <form>
-        <label>
-          Top text:
-          <input
-            value={topText}
-            onChange={(event) => {
-              setTopText(event.target.value);
-            }}
+    <main>
+      <div className="GeneratorButton">
+        <h1>Meme Generator</h1>
+        <p>Juice your fresh picture !</p>
+        <form>
+          <div className="meme template">
+            <label className="template">
+              Meme template:
+              <select
+                /* select the option from dropdown */
+                value={meme}
+                onClick={clearText}
+                onChange={(e) => {
+                  setMeme(e.target.value);
+                  generateMeme(e);
+                }}
+                onKeyDown={(e) => {
+                  setMeme(e.target.value);
+                  generateMeme(e);
+                }}
+              >
+                {/* Dropdown options */}
+                {options.map((templates) => (
+                  <option
+                    key={`templates-${templates.id}`}
+                    value={templates.id}
+                  >
+                    {templates.id}
+                  </option>
+                ))}
+              </select>
+            </label>
+            {/* alternative random meme button */}
+            {/* <button onClick={randomImage}>Random Meme</button> */}
+          </div>
+        </form>
+        <section className="text input">
+          <div className="text boxes">
+            <form>
+              <label>
+                Top text:
+                <input
+                  size="54"
+                  value={topText}
+                  onChange={(event) => {
+                    setTopText(event.target.value);
+                  }}
+                />
+              </label>
+            </form>
+
+            <form>
+              <label>
+                Bottom text:
+                <input
+                  size="50"
+                  value={bottomText}
+                  onChange={(event) => {
+                    setBottomText(event.target.value);
+                  }}
+                />
+              </label>
+            </form>
+          </div>
+        </section>
+
+        <section>
+          <button type="button" onClick={generateMeme}>
+            Generate Meme
+          </button>
+          <button onClick={downloadMeme}>Download Meme</button>
+        </section>
+        <div className="Meme Image">
+          <img
+            data-test-id="meme-image"
+            className="finalMeme"
+            src={finalMeme}
+            alt="Meme"
           />
-        </label>
-      </form>
-      <form>
-        <label>
-          Bottom text:
-          <input
-            value={bottomText}
-            onChange={(event) => {
-              setBottomText(event.target.value);
-            }}
-          />
-        </label>
-      </form>
-      <form>
-        <div>
-          Meme Templates:
-          <select
-            defaultValue={meme}
-            value={meme}
-            onChange={(e) => {
-              setMeme(e.target.value);
-            }}
-          >
-            <option value="doge">Doge</option>
-            <option value="captain">Captain</option>
-            <option value="depressed">Depressed</option>
-            <option value="grumpy">Grumpy</option>
-            <option value="honest">Honest</option>
-            <option value="kermit">Kermit</option>
-            <option value="noone">Noone</option>
-            <option value="scared">Scared</option>
-            <option value="wonka">Wonka</option>
-          </select>
         </div>
-      </form>
-      <button type="button" onClick={generateMeme}>
-        Generate Meme
-      </button>
-      <div className="Meme">
-        <img src={finalMeme} alt="Meme" />
       </div>
-    </div>
+    </main>
   );
 }
